643a5cd53589fc611ee255fe345bcebe933f7e83
Windy Player <windyplayer@gmx.com>
2025-04-15 11:03:21 -0700

    Change the column setup based on a JavaFx book.

--- src/main/java/net/windyweather/pairtable/PairTableController.java
+++ src/main/java/net/windyweather/pairtable/PairTableController.java
@@ -1,5 +1,6 @@
 package net.windyweather.pairtable;
 
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -8,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 
 import java.util.Set;
 
@@ -17,6 +19,13 @@ public class PairTableController {
     public TableColumn<SyncFilesPair, String> tcStatus;
     @FXML
     private Label lblStatus;
+
+    //
+    // Do this in one place so we can easily turn it off later
+    //
+    public static void printSysOut( String str ) {
+        System.out.println(str);
+    }
 
     void SetStatus( String sts ) {
         lblStatus.setText( sts );
@@ -34,8 +43,28 @@ public class PairTableController {
             What are the strings in the following calls for?
             Do they need to match the property names in the SyncFilesPair class?
          */
-        tcName.setCellValueFactory( new PropertyValueFactory<SyncFilesPair, String>("sPairName"));
-        tcStatus.setCellValueFactory( new PropertyValueFactory<SyncFilesPair, String>("sPairStatus") );
+        //tcName.setCellValueFactory( new PropertyValueFactory<SyncFilesPair, String>("sPairName"));
+        //tcStatus.setCellValueFactory( new PropertyValueFactory<SyncFilesPair, String>("sPairStatus") );
+
+
+        printSysOut("Set up CellValueFactories for Columns");
+
+        tcName.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<SyncFilesPair, String>, ObservableValue<String>>() {
+            public ObservableValue<String> call( TableColumn.CellDataFeatures<SyncFilesPair,
+                                String> p) {
+                printSysOut("tcName CellValueFactory called");
+                return p.getValue().sPairNameProperty();
+            }
+        });
+
+        tcStatus.setCellValueFactory( new Callback<TableColumn.CellDataFeatures<SyncFilesPair, String>, ObservableValue<String>>() {
+            public ObservableValue<String> call( TableColumn.CellDataFeatures<SyncFilesPair,
+                    String> p) {
+                printSysOut("tcStatus CellValueFactory called");
+                return p.getValue().sPairStatusProperty();
+            }
+        });
+
     }
 
 
@@ -58,11 +87,13 @@ public class PairTableController {
     }
 
         public void OnAddPairs(ActionEvent actionEvent) {
+            printSysOut("OnAddPairs - add some pairs");
             SomeTestPairData();
             SetStatus("Pairs Added");
     }
 
     public void OnRemovePairs(ActionEvent actionEvent) {
+        printSysOut("OnRemovePairs - remove all the pairs");
         pairObservableList.clear();
         SetStatus("Pairs Removed");
     }
